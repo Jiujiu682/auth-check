a69fc52dc5560c7ba53b7252a53e348361ac3a23
Jiujiu682 <3984741436@qq.com>
2026-06-02 21:43:32 +0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -2,35 +2,19 @@ const CryptoJS = require("crypto-js");
 const fs = require("fs");
 const path = require("path");
 const KEY = "abc1234567890key";
-const dbPath = path.join(__dirname, "db.json");
+const db = path.join(__dirname, "db.json");
+const load = () => fs.existsSync(db) ? JSON.parse(fs.readFileSync(db, "utf8")) : [];
+const dec = s => JSON.parse(CryptoJS.AES.decrypt(s, KEY).toString(CryptoJS.enc.Utf8));
+const enc = o => CryptoJS.AES.encrypt(JSON.stringify(o), KEY).toString();
 
-function loadDB() {
-  if (!fs.existsSync(dbPath)) {
-    fs.writeFileSync(dbPath, "[]");
-    return [];
-  }
-  return JSON.parse(fs.readFileSync(dbPath, "utf8"));
-}
-
-//解密
-function decrypt(str) {
-  let raw = CryptoJS.AES.decrypt(str, KEY);
-  return JSON.parse(raw.toString(CryptoJS.enc.Utf8));
-}
-//加密
-function encrypt(obj) {
-  return CryptoJS.AES.encrypt(JSON.stringify(obj), KEY).toString();
-}
-
-module.exports = async (req, res) => {
-  const db = loadDB();
-  let body = decrypt(req.body.payload);
-  let list = db.filter(item => {
-    let ok = true;
-    if (body.key && !item.code.includes(body.key)) ok = false;
-    if (body.use && item.useTime !== body.use) ok = false;
-    if (body.ban && item.ban !== body.ban) ok = false;
-    return ok;
+module.exports = (req, res) => {
+  let d = dec(req.body.payload);
+  let list = load().filter(x => {
+    let f = true;
+    if (d.key && !x.code.includes(d.key)) f = false;
+    if (d.use && x.useTime != d.use) f = false;
+    if (d.ban && x.ban != d.ban) f = false;
+    return f;
   })
-  res.json({ payload: encrypt(list) });
+  res.json({ payload: enc(list) });
 }
