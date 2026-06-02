734722bd4036c7a341b6f23119102406e632b105
Jiujiu682 <3984741436@qq.com>
2026-06-02 22:53:08 +0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,17 +1,7 @@
-import { createClient } from '@upstash/redis'
-const redis = createClient({
-  url:"https://trusted-mayfly-113263.upstash.io",
-  token:"gQAAAAAAAbpvAAIgcDExNDY2NDlkYWZlMTA0YzIxYWVkNjlhYmEzNzJmMmM3ZQ"
-})
-
-export default async function handler(req,res){
-  res.setHeader("Access-Control-Allow-Origin","*")
-  if(req.method!=='POST') return res.json({ok:false})
-  const {key}=req.body
-  //固定测试卡密
-  if(key==='ceshi123'){
-    return res.json({ok:true})
-  }else{
-    return res.json({ok:false})
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  if (req.method === "POST" && req.body.key === "ceshi123") {
+    return res.json({ ok: true })
   }
+  return res.json({ ok: false })
 }
