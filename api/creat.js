c285410f1f173373d82ea8845aa5e8f828bec921
Jiujiu682 <3984741436@qq.com>
2026-06-02 21:44:46 +0800

    Update creat.js

--- api/creat.js
+++ api/creat.js
@@ -2,50 +2,26 @@ const CryptoJS = require("crypto-js");
 const fs = require("fs");
 const path = require("path");
 const KEY = "abc1234567890key";
-const dbPath = path.join(__dirname, "db.json");
-
-function loadDB() {
-  if (!fs.existsSync(dbPath)) {
-    fs.writeFileSync(dbPath, "[]");
-    return [];
-  }
-  return JSON.parse(fs.readFileSync(dbPath, "utf8"));
-}
-function saveDB(arr) {
-  fs.writeFileSync(dbPath, JSON.stringify(arr, null, 2));
-}
-function decrypt(str) {
-  let raw = CryptoJS.AES.decrypt(str, KEY);
-  return JSON.parse(raw.toString(CryptoJS.enc.Utf8));
-}
-function encrypt(obj) {
-  return CryptoJS.AES.encrypt(JSON.stringify(obj), KEY).toString();
-}
-//生成32位卡密
-function makeCode() {
-  let s = "", ch = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopq1234567890";
-  for (let i = 0; i < 32; i++) s += ch[Math.floor(Math.random() * ch.length)];
+const db = path.join(__dirname, "db.json");
+const load = () => fs.existsSync(db) ? JSON.parse(fs.readFileSync(db, "utf8")) : [];
+const save = d => fs.writeFileSync(db, JSON.stringify(d, null, 2));
+const dec = s => JSON.parse(CryptoJS.AES.decrypt(s, KEY).toString(CryptoJS.enc.Utf8));
+const enc = o => CryptoJS.AES.encrypt(JSON.stringify(o), KEY).toString();
+const rand = () => {
+  let s = "", c = "ABCDEFGabcdefg1234567890";
+  for (let i = 32; i--; ) s += c[Math.floor(Math.random() * c.length)];
   return s;
 }
 
-module.exports = async (req, res) => {
-  let db = loadDB();
-  let data = decrypt(req.body.payload);
-  let { num, type, day } = data;
-  let addArr = [];
-  let now = new Date().toLocaleString();
-  for (let i = 0; i < num; i++) {
-    addArr.push({
-      code: makeCode(),
-      type: type,
-      useTime: "未使用",
-      ban: "正常",
-      day: day,
-      admin: "后台生成",
-      create: now
-    })
+module.exports = (req, res) => {
+  let arr = load();
+  let d = dec(req.body.payload);
+  let add = [];
+  let t = new Date().toLocaleString();
+  for (let i = d.num; i--; ) {
+    add.push({ code: rand(), type: d.type, useTime: "未使用", ban: "正常", day: d.day, create: t })
   }
-  db.push(...addArr);
-  saveDB(db);
-  res.json({ payload: encrypt({ msg: `成功生成${num}张`, list: addArr }) });
+  arr.push(...add);
+  save(arr);
+  res.json({ payload: enc({ msg: `生成${d.num}张`, list: add }) });
 }
