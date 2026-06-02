c723df2157bdc60933cbdbf4d9d91de824a37995
Jiujiu682 <3984741436@qq.com>
2026-06-02 23:02:34 +0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,14 +1,14 @@
 import { createClient } from '@upstash/redis'
 import crypto from 'crypto'
 
-//这里填你不带引号的URL和TOKEN
 const redis = createClient({
   url: "https://trusted-mayfly-113263.upstash.io",
   token: "gQAAAAAAAbpvAAIgcDExNDY2NDlkYWZlMTA0YzIxYWVkNjlhYmEzNzJmMmM3ZQ"
 })
 const SECRET_SALT = "sk5689xd2026#1t"
-//新增卡密在这里
+//测试卡密：ceshi123（1天时效），加卡密就在这行添加 ["新卡密",天数],
 const keyPool = [["ceshi123",1]]
+
 const encryptKey = (str)=>crypto.createHmac('md5',SECRET_SALT).update(str).digest('hex')
 
 export default async function handler(req,res){
@@ -17,12 +17,14 @@ export default async function handler(req,res){
     if(req.method !== "POST") return res.json({ok:false})
     const {key}=req.body
     const md5Key = encryptKey(key)
+
     let blackList = await redis.get('usedBlackList')
     blackList = Array.isArray(blackList)?blackList:[]
 
     if(blackList.includes(md5Key)) return res.json({ok:false})
     const expire = await redis.get(`active:${md5Key}`)
     const now = new Date()
+
     if(expire){
       const end = new Date(expire)
       if(end <= now){
@@ -33,19 +35,21 @@ export default async function handler(req,res){
       }
       return res.json({ok:true})
     }
+
     let day=0
     for(let [k,d] of keyPool){
       if(encryptKey(k)===md5Key) day=d
     }
     if(day===0) return res.json({ok:false})
+
     let endDate = new Date()
     endDate.setDate(endDate.getDate()+day)
     await redis.set(`active:${md5Key}`,endDate.toString())
     blackList.push(md5Key)
     await redis.set('usedBlackList',blackList)
+
     return res.json({ok:true})
   }catch(e){
-    //报错直接返回false，不会出现500页面
     return res.json({ok:false})
   }
 }
