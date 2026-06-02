4c9bfa53ff24c3a42d6a5a9209db6133f01a15a5
Jiujiu682 <3984741436@qq.com>
2026-06-02 21:48:47 +0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,20 +1,16 @@
 const CryptoJS = require("crypto-js");
-const fs = require("fs");
-const path = require("path");
 const KEY = "abc1234567890key";
-const db = path.join(__dirname, "db.json");
-const load = () => fs.existsSync(db) ? JSON.parse(fs.readFileSync(db, "utf8")) : [];
-const dec = s => JSON.parse(CryptoJS.AES.decrypt(s, KEY).toString(CryptoJS.enc.Utf8));
-const enc = o => CryptoJS.AES.encrypt(JSON.stringify(o), KEY).toString();
-
-module.exports = (req, res) => {
-  let d = dec(req.body.payload);
-  let list = load().filter(x => {
-    let f = true;
-    if (d.key && !x.code.includes(d.key)) f = false;
-    if (d.use && x.useTime != d.use) f = false;
-    if (d.ban && x.ban != d.ban) f = false;
-    return f;
+global.cardList = global.cardList || [];
+const decode = s => JSON.parse(CryptoJS.AES.decrypt(s, KEY).toString(CryptoJS.enc.Utf8));
+const encode = d => CryptoJS.AES.encrypt(JSON.stringify(d), KEY).toString();
+module.exports = (req,res)=>{
+  let p = decode(req.body.payload);
+  let arr = global.cardList.filter(item=>{
+    let flag = true;
+    if(p.key&&!item.code.includes(p.key)) flag=false;
+    if(p.use&&item.useTime!==p.use) flag=false;
+    if(p.ban&&item.ban!==p.ban) flag=false;
+    return flag;
   })
-  res.json({ payload: enc(list) });
+  res.json({payload:encode(arr)})
 }
