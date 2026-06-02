83c2ad3da4e4b0a380cf23b2363b54bdec2bf939
Jiujiu682 <3984741436@qq.com>
2026-06-02 21:11:58 +0800

    Create del.js

---
+++ api/del.js
@@ -0,0 +1,27 @@
+const { webcrypto } = require('node:crypto');
+const SEC = 'abc1234567890key';
+
+async function getKey(){
+  return webcrypto.subtle.importKey('raw',Buffer.from(SEC),{name:'AES-CBC'},false,['encrypt','decrypt'])
+}
+async function aesDec(str){
+  const k=await getKey();
+  const b=Buffer.from(str,'base64');
+  const iv=b.slice(0,16);
+  const data=b.slice(16);
+  const raw=await webcrypto.subtle.decrypt({name:'AES-CBC',iv},k,data);
+  return JSON.parse(Buffer.from(raw).toString());
+}
+async function aesEnc(obj){
+  const k=await getKey();
+  const iv=webcrypto.getRandomValues(new Uint8Array(16));
+  const buf=Buffer.from(JSON.stringify(obj));
+  const raw=await webcrypto.subtle.encrypt({name:'AES-CBC',iv},k,buf);
+  return Buffer.concat([iv,Buffer.from(raw)]).toString('base64');
+}
+
+module.exports=async(req,res)=>{
+  const {keys}=await aesDec(req.body.payload);
+  global.keyDB = global.keyDB.filter(x=>!keys.includes(x.code));
+  res.json({payload:await aesEnc({msg:'ok'})})
+}
