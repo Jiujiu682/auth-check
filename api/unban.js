11b5c8a0dea5fc9d4bb6ab6b4354fd4bcd2f616d
Jiujiu682 <3984741436@qq.com>
2026-06-02 21:49:56 +0800

    Update unban.js

--- api/unban.js
+++ api/unban.js
@@ -1,17 +1,10 @@
 const CryptoJS = require("crypto-js");
-const fs = require("fs");
-const path = require("path");
 const KEY = "abc1234567890key";
-const db = path.join(__dirname, "db.json");
-const load = () => fs.existsSync(db) ? JSON.parse(fs.readFileSync(db, "utf8")) : [];
-const save = d => fs.writeFileSync(db, JSON.stringify(d, null, 2));
-const dec = s => JSON.parse(CryptoJS.AES.decrypt(s, KEY).toString(CryptoJS.enc.Utf8));
-const enc = o => CryptoJS.AES.encrypt(JSON.stringify(o), KEY).toString();
-
-module.exports = (req, res) => {
-  let arr = load();
-  let d = dec(req.body.payload);
-  arr.forEach(x => d.keys.includes(x.code) && (x.ban = "正常"));
-  save(arr);
-  res.json({ payload: enc({ msg: "ok" }) });
+global.cardList = global.cardList || [];
+const decode = s => JSON.parse(CryptoJS.AES.decrypt(s, KEY).toString(CryptoJS.enc.Utf8));
+const encode = d => CryptoJS.AES.encrypt(JSON.stringify(d), KEY).toString();
+module.exports = (req,res)=>{
+  let {keys}=decode(req.body.payload);
+  global.cardList.forEach(v=>{if(keys.includes(v.code))v.ban="正常"});
+  res.json({payload:encode({msg:"ok"})})
 }
